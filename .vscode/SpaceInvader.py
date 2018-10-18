40fa88cd6e737dd45fd5e20ad6a8dc4c829dc706
Evan <rekreider@una.edu>
2018-10-18 10:12:26 -0500

    Added printed title

--- .vscode/SpaceInvader.py
+++ .vscode/SpaceInvader.py
@@ -18,6 +18,8 @@ pygame.init()
 gameDisplay = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption('Space Invaders')
 
+titleFont = pygame.font.SysFont('Aurebesh Condensed', 30, False)
+
 clock = pygame.time.Clock()
 
 playerImg = pygame.image.load("si-player.gif")
@@ -76,6 +78,9 @@ while isAlive:
     gameDisplay.blit(backgroundImg, (gameSideMargin + gameBorderWidth, gameTopMargin + gameBorderWidth), (0, 0, gameWidth, gameHeight))
 
     player.show()
+
+    titleText = titleFont.render('SPACE INVADERS', False, white)
+    gameDisplay.blit(titleText, (windowWidth / 2 - titleText.get_width() / 2, 0))
 
     clock.tick(60)
     pygame.display.update()
